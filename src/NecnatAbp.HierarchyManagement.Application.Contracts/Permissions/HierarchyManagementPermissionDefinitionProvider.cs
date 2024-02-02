442b52fd5a9f0a02eba833e783b0608aead1f44a
Evandro Mota de Souza <evandro.ems@hotmail.com>
2024-02-02 02:22:29 -0300

    Permissions s

--- src/NecnatAbp.HierarchyManagement.Application.Contracts/Permissions/HierarchyManagementPermissionDefinitionProvider.cs
+++ src/NecnatAbp.HierarchyManagement.Application.Contracts/Permissions/HierarchyManagementPermissionDefinitionProvider.cs
@@ -10,24 +10,24 @@ public class HierarchyManagementPermissionDefinitionProvider : PermissionDefinit
     {
         var myGroup = context.AddGroup(HierarchyManagementPermissions.GroupName, L("Permission:HierarchyManagement"));
 
-        var pgHierarchy = myGroup.AddPermission(HierarchyManagementPermissions.Hierarchies.Default, L("Permission:Hierarchy:Default"));
-        pgHierarchy.AddChild(HierarchyManagementPermissions.Hierarchies.Create, L("Permission:Hierarchy:Create"));
-        pgHierarchy.AddChild(HierarchyManagementPermissions.Hierarchies.Update, L("Permission:Hierarchy:Edit"));
-        pgHierarchy.AddChild(HierarchyManagementPermissions.Hierarchies.Delete, L("Permission:Hierarchy:Delete"));
+        var pgHierarchy = myGroup.AddPermission(HierarchyManagementPermissions.Hierarchies.Default, L("Permission:Hierarchies:Default"));
+        pgHierarchy.AddChild(HierarchyManagementPermissions.Hierarchies.Create, L("Permission:Hierarchies:Create"));
+        pgHierarchy.AddChild(HierarchyManagementPermissions.Hierarchies.Update, L("Permission:Hierarchies:Edit"));
+        pgHierarchy.AddChild(HierarchyManagementPermissions.Hierarchies.Delete, L("Permission:Hierarchies:Delete"));
 
-        var pgHierarchyComponentGroup = myGroup.AddPermission(HierarchyManagementPermissions.HierarchyComponentGroups.Default, L("Permission:HierarchyComponentGroup:Default"));
-        pgHierarchyComponentGroup.AddChild(HierarchyManagementPermissions.HierarchyComponentGroups.Create, L("Permission:HierarchyComponentGroup:Create"));
-        pgHierarchyComponentGroup.AddChild(HierarchyManagementPermissions.HierarchyComponentGroups.Update, L("Permission:HierarchyComponentGroup:Edit"));
-        pgHierarchyComponentGroup.AddChild(HierarchyManagementPermissions.HierarchyComponentGroups.Delete, L("Permission:HierarchyComponentGroup:Delete"));
+        var pgHierarchyComponentGroup = myGroup.AddPermission(HierarchyManagementPermissions.HierarchyComponentGroups.Default, L("Permission:HierarchyComponentGroups:Default"));
+        pgHierarchyComponentGroup.AddChild(HierarchyManagementPermissions.HierarchyComponentGroups.Create, L("Permission:HierarchyComponentGroups:Create"));
+        pgHierarchyComponentGroup.AddChild(HierarchyManagementPermissions.HierarchyComponentGroups.Update, L("Permission:HierarchyComponentGroups:Edit"));
+        pgHierarchyComponentGroup.AddChild(HierarchyManagementPermissions.HierarchyComponentGroups.Delete, L("Permission:HierarchyComponentGroups:Delete"));
 
-        var pgHierarchicalStructure = myGroup.AddPermission(HierarchyManagementPermissions.HierarchicalStructures.Default, L("Permission:HierarchicalStructure:Default"));
-        pgHierarchicalStructure.AddChild(HierarchyManagementPermissions.HierarchicalStructures.Create, L("Permission:HierarchicalStructure:Create"));
-        pgHierarchicalStructure.AddChild(HierarchyManagementPermissions.HierarchicalStructures.Delete, L("Permission:HierarchicalStructure:Delete"));
+        var pgHierarchicalStructure = myGroup.AddPermission(HierarchyManagementPermissions.HierarchicalStructures.Default, L("Permission:HierarchicalStructures:Default"));
+        pgHierarchicalStructure.AddChild(HierarchyManagementPermissions.HierarchicalStructures.Create, L("Permission:HierarchicalStructures:Create"));
+        pgHierarchicalStructure.AddChild(HierarchyManagementPermissions.HierarchicalStructures.Delete, L("Permission:HierarchicalStructures:Delete"));
 
-        var pgUserRoleHierarchicalStructure = myGroup.AddPermission(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Default, L("Permission:UserRoleHierarchicalStructure:Default"));
-        pgUserRoleHierarchicalStructure.AddChild(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Create, L("Permission:UserRoleHierarchicalStructure:Create"));
-        pgUserRoleHierarchicalStructure.AddChild(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Update, L("Permission:UserRoleHierarchicalStructure:Edit"));
-        pgUserRoleHierarchicalStructure.AddChild(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Delete, L("Permission:UserRoleHierarchicalStructure:Delete"));
+        var pgUserRoleHierarchicalStructure = myGroup.AddPermission(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Default, L("Permission:UserRoleHierarchicalStructures:Default"));
+        pgUserRoleHierarchicalStructure.AddChild(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Create, L("Permission:UserRoleHierarchicalStructures:Create"));
+        pgUserRoleHierarchicalStructure.AddChild(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Update, L("Permission:UserRoleHierarchicalStructures:Edit"));
+        pgUserRoleHierarchicalStructure.AddChild(HierarchyManagementPermissions.UserRoleHierarchicalStructures.Delete, L("Permission:UserRoleHierarchicalStructures:Delete"));
     }
 
     private static LocalizableString L(string name)
